a8878a0380ded21da518f4e7ac806c9598dd3e33
Warheart-1 <axel97477@gmail.com>
2023-01-13 12:36:26 +0100

    first init

---
+++ SuperSimonProject/src/main.ts
@@ -0,0 +1,14 @@
+export class SuperSimonProject {
+    constructor() {
+        console.log('Super Simon Project');
+    }
+
+    public start() {
+        console.log('Start');
+    }
+
+    public stop() {
+        console.log('Stop');
+    }  
+}
+
