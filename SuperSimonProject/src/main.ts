21f816ef8aa9accea10bb45cd400173ab0892186
Warheart-1 <axel97477@gmail.com>
2023-01-18 08:02:36 +0100

    update (add sound)

--- SuperSimonProject/src/main.ts
+++ SuperSimonProject/src/main.ts
@@ -1,21 +1,43 @@
+import * as Tone from 'tone';
+
+ type button = {
+  color: string;
+  tone: string;
+ }
+
 export class SuperSimon {
-  public buttons : string[] = ['red', 'blue', 'green', 'yellow'];
+  public buttons : button[] = [
+    {color: 'red', tone: "C4"},
+    {color: 'blue', tone: "D4"},
+    {color: 'green', tone: "E4"},
+    {color: 'yellow', tone: "F4"},
+  ];
+
   public buttonStart: string = 'start';
-  public sequence = Array<string>();
   public speed = 2500;
-  public constructor() {
+  private sequence = Array<string>();
+  private lastSequence = Array<string>();
+  private _synth : Tone.Synth<Tone.SynthOptions>;
+  
+  public constructor(synth : Tone.Synth<Tone.SynthOptions>) {
     this.buttons.forEach(button => {
-      document.getElementById(button)?.addEventListener('click', () => {
-        this.buttonClicked(button);
+      document.getElementById(button.color)?.addEventListener('click', () => {
+        this.buttonClicked(button.color);
       });
     });
-    document.getElementById(this.buttonStart)?.addEventListener('click', () => {
+    document.getElementById(this.buttonStart)?.addEventListener('click', (e : MouseEvent) => {
+      (e.target as HTMLButtonElement).disabled = true;
       this.init();
     });
+    this._synth = synth;
   }
 
-  private init() {
-    const numberOfExercises = Math.floor(Math.random() * 10) + 1;
+  public init() {
+    const numberOfExercises = Math.floor(Math.random() * 1) + 1;
+    if(this.lastSequence.length > 0) {
+      this.sequence.push(...this.lastSequence);
+      this.lastSequence = [];
+    }
     for (let i = 0; i < numberOfExercises; i++) {
       this.addNextButton();
     }
@@ -23,49 +45,76 @@ export class SuperSimon {
       this.playSequence();
     }
     console.log(this.sequence);
-    console.log(numberOfExercises);
+    console.log(numberOfExercises)
+  }
+
+  private addNextButton() {
+    const randomIndex = Math.floor(Math.random() * this.buttons.length);
+    this.sequence.push(this.buttons[randomIndex].color);
   }
 
   private playSequence() {
+    this.buttons.forEach(button => {
+      (document.getElementById(button.color) as HTMLButtonElement).disabled = true;
+    });
     this.sequence.forEach((button, index) => {
       setTimeout(() => {
         this.playButton(button);
       }, this.speed * index);
     });
+    setTimeout(() => {
+      this.buttons.forEach(button => {
+        (document.getElementById(button.color) as HTMLButtonElement).disabled = false;
+      });
+    } , this.speed * this.sequence.length);
+  }
+
+  private playSound(button: string) {
+    const buttonElement = this.buttons.find(b => b.color === button);
+    if (buttonElement) {
+      this._synth.triggerAttackRelease(buttonElement.tone, "8n");
+    }
   }
 
   private playButton(button: string) {
     const buttonElement = document.getElementById(button);
     if (buttonElement) {
-      buttonElement.classList.add('active');
+      this.playSound(button);
+      buttonElement.classList.add(`${button}-active`);
       setTimeout(() => {
-        buttonElement.classList.remove('active');
+        buttonElement.classList.remove(`${button}-active`);
       }, this.speed-100);
     }
-  }
-  
-  private addNextButton() {
-    const randomIndex = Math.floor(Math.random() * this.buttons.length);
-    this.sequence.push(this.buttons[randomIndex]);
   }
 
   private buttonClicked(button: string) {
     if (this.sequence.length > 0) {
       if (button === this.sequence[0]) {
+        this.playSound(button);
+        this.lastSequence.push(button);
         this.sequence.shift();
         if (this.sequence.length === 0) {
           if (this.speed > 500) {
             this.speed = this.speed - 500;
           } 
-          this.init();
+
+          setTimeout(() => {
+            this.init();
+          }, 500);
         }
       } else {
-        this.sequence = [];
-        alert('Game over');
+        this.gameOver();
       }
     }
   }
+  
+  public gameOver() {
+    this.sequence = [];
+    alert('Game over');
+    const buttonStart = document.getElementById(this.buttonStart) as HTMLButtonElement;
+    buttonStart.textContent = 'Restart';
+    buttonStart.disabled = false;
+  }
 }
-
-const superSimon = new SuperSimon();
-
+const synth = new Tone.Synth().toDestination();
+const superSimon = new SuperSimon(synth);
